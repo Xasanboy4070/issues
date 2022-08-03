8feb589100c157a4aa7cf3af20487a0746fbd494
xasanboy <xasanboyorifjonov2@gmail.com>
2022-08-03 15:58:00 +0500

    nothing

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Issues from "./components/issues";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Issues/>);
