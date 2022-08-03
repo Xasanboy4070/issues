679b89bbd60fe00f8c7b2cc4fc323686b0d38a86
scamlux <scamlux@gmail.com>
2022-08-03 18:10:26 +0500

    new styles&working but not responsive, statega qoyildi

--- src/components/issues.jsx
+++ src/components/issues.jsx
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import Issue from "../components/issue";
 import getIssues from "../utils/get-users.js";
 class Issues extends Component {
-  sues = getIssues(20);
+  state = {
+    sues: getIssues(20),
+  };
   render() {
-    console.log(this.sues);
+    console.log(this.state.sues);
     return (
       <div className="main">
-        {this.sues.map((issue, idx) => (
+        {this.state.sues.map((issue, idx) => (
           <Issue sue={issue} key={idx} />
         ))}
       </div>
